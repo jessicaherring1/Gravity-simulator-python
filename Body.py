b3409236d96ac283fe4b399cef1598c1f35342e4
jessicaherring1 <jessica.herring123@gmail.com>
2022-03-20 21:29:15 -0400

    lots of updates....

--- Body.py
+++ Body.py
@@ -1,41 +1,44 @@
+from turtle import position
+from cv2 import sqrt
 import pygame 
 import random
 import math
 
 class Body:
-    vx = 0 #change the intial to a different number 
-    vy = 0 #change the intial to a different number 
-    ax = 0
-    ay = 0
-    #x = 0
-    #y = 0
+    px = 0 #x position
+    py = 0 #y position
+    vx = 0 #x velocity 
+    vy = 0 #y velocity
+    ax = 0 #x acceleration
+    ay = 0 #y acceleration 
 
-    radius = 20
+    radius = 0
+    mass =0
     angle = 0
     distance = 0
     orbitSpeed = 15
+    bodies=[]
 
-
-    width = 20
-    height = 20
-    speed = 0
     isHit = False
     topBound = 0
     bottomBound = 0
     leftBound = 0
     rightBound = 0
 
-    def __init__(self, tempr, tempd, tempo):
+    def __init__(self, tempr, tempmass, temppx=0, temppy=0, tempvx=0, tempvy=0,):
         self.radius = tempr
-        self.distance = tempd
-        self.orbitSpeed = tempo
+        self.mass = tempmass
+        self.px=temppx
+        self.py = temppy
+        self.vx=tempvx
+        self.vy = tempvy
         degree = random.randint(0, 360)
-        angle = (degree * 2 * math.pi) / 360
+        self.angle = (degree * 2 * math.pi) / 360
+        self.add_body(self)
     
     def render(self, aSurface):
-        #body = pygame.Rect(self.x, self.y, self.width, self.height)
-        #pygame.draw.rect(aSurface, (255, 0, 255), body)
-        pygame.draw.circle(aSurface, (255, 0, 255), (600, 600), self.radius)
+        pygame.draw.circle(aSurface, (255, 0, 255), (self.px, self.py), self.radius)
+    
     def resetBoundaries(self): #incomplete
         self.topBound = self.y
         self.bottomBound = self.y + self.height
@@ -44,3 +47,97 @@ class Body:
         #angle += orbitSpeed
         pass
 
+    def move(self):
+        self.px = self.px + self.vx
+        self.py = self.py + self.vy
+
+    # functions used/adapted from 
+    # https://thepythoncodingbook.com/2021/09/29/simulating-orbiting-planets-in-a-solar-system-using-python-orbiting-planets-series-1/
+    def add_body(self, body):
+        self.bodies.append(body)
+
+    def remove_body(self, body):
+        self.bodies.remove(body)
+
+    #both arguments must be a Body object
+    '''
+    def acceleration(body1, body2):
+        tempDist = math.dist((body1.px, body1.py), (body2.px, body2.py))
+        force = body1.mass * body2.mass / tempDist**2
+        angle = math.atan2((body1.py - body2.py),(body1.px - body2.px))
+        reverse = 1
+
+        for body in body1, body2:
+            acceleration = force / body.mass
+            body.ax = acceleration * math.cos(math.radians(angle))
+            body.ay = acceleration * math.sin(math.radians(angle))
+            body.vx = body.vx + body.ax
+            body.vy = body.vy + body.ay
+            reverse = -1'''
+
+    def acceleration(self, body2):
+        dx = self.px-body2.px # change in two x-positions
+        dy = self.py - body2.py # change in two y-positions
+        d = math.sqrt(dx**2 + dy**2)
+        
+        force = (self.mass * body2.mass) / d**2
+        theta = math.atan2(dy,dx)
+
+        fx = math.cos(math.radians(theta)) * force
+        fy = math.sin(math.radians(theta)) * force
+
+        self.ax = (fx / self.mass) #* -1
+        self.ay = (fy / self.mass)#*-1
+        #print(self.ax, self.ay)
+        
+        self.vx += self.ax 
+        self.vy += self.ay 
+        self.px += self.vx
+        self.py += self.vy
+        
+        body2.ax = (fx / body2.mass)*-1
+        body2.ay = (fy / body2.mass)*-1
+        #print(body2.ax, body2.ay)
+
+        body2.vx += body2.ax
+        body2.vy += body2.ay
+        body2.px += body2.vx
+        body2.py += body2.vy 
+        '''
+        self.ax = self.mass*(body2.px-self.px) / tempDist**2
+        self.ay = self.mass*(body2.py-self.py) / tempDist**2
+
+        body2.ax = body2.mass*(self.px-body2.px) / tempDist**2
+        body2.ay = body2.mass*(self.py-body2.py) / tempDist**2
+
+        self.vx = self.vx + self.ax
+        self.vy = self.vy + self.ay
+        self.px = self.px + self.vx
+        self.py = self.py + self.vy
+
+        body2.vx = body2.vx + (body2.ax * -1)
+        body2.vy = body2.vy + (body2.ay * -1)
+        body2.px = body2.px + body2.vx
+        body2.py = body2.py + body2.vy
+
+        selfAcceleration = force / self.mass
+        self.ax = selfAcceleration * math.cos(math.radians(angle))
+        self.ay = selfAcceleration * math.sin(math.radians(angle))
+        self.vx = self.vx + self.ax
+        self.vy = self.vy + self.ay
+        self.px = self.px + self.vx
+        self.py = self.py + self.vy
+        reverse = -1
+
+        body2Acceleration = force / body2.mass
+        body2.ax = body2Acceleration * math.cos(math.radians(angle))
+        body2.ay = body2Acceleration * math.sin(math.radians(angle))
+        body2.vx = body2.vx + body2.ax
+        body2.vy = body2.vy + body2.ay
+        body2.px = body2.px + body2.vx
+        body2.py = body2.py + body2.vy
+        reverse = 1'''
+
+
+    
+    
--- main.py
+++ main.py
@@ -3,17 +3,30 @@ from cv2 import circle
 import pygame
 from Body import Body
 from HandDetector import HandDetector
+import math
+
+import random
 
 
-
-WIDTH = 1200
-HEIGHT = 800
+WIDTH = 1200 #window width
+HEIGHT = 800 #window height
+size = (WIDTH, HEIGHT)
+screen = pygame.display.set_mode(size)
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+bg = pygame.image.load("background.jpg").convert()
+bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
+
 pygame.display.set_caption("Gravity Simulator")
 FPS=60
 
-planet = Body(15, 100, 15)
+planet = Body(30, 10, 600, 200, 2, 2)
+planet1 = Body(60, 10000, 600, 400)
+
+collision = False
+
+def mapToNewRange(val, inputMin, inputMax, outputMin, outputMax):
+    return outputMin + ((outputMax - outputMin) / (inputMax - inputMin)) * (val - inputMin)
 
 def main():
     clock = pygame.time.Clock()
@@ -22,25 +35,116 @@ def main():
 # make a boolean that represents whether the game should continue to run or not
     running = True
 
+    #circle Vars
+    circleX = 0
+    circleY = 0
+    circleZ =0
+    circleC = (0, 255, 255)
+
+    handIsOpen=True
 
     # while the game is running
-    while running:
+    while not handDetector.shouldClose and running:
         # this makes it so this function can run at most FPS times/sec
+        handDetector.update()
+
         clock.tick(FPS)
+ 
+        screen.blit(bg, [0,0])
+
+        if len(handDetector.landmarkDictionary) > 0: 
+            #print(handDetector.landmarkDictionary[0])
+            circleX = handDetector.landmarkDictionary[0][9][0]
+            circleY = handDetector.landmarkDictionary[0][9][1]
+            #circleZ = handDetector.landmarkDictionary[0][9][2]
+            #circleZ = abs(circleZ *5)
+            circleZ = 30
+
+            #mirror circleX/Y so its more intuitive
+            circleX = WIDTH - mapToNewRange(circleX, 0, 640, 0, WIDTH)
+            circleY = mapToNewRange(circleY, 0, 480, 0, HEIGHT)
+
+            #draw circle at point 9
+            pygame.draw.circle(WINDOW, (255, 0, 255), (circleX, circleY), circleZ)
+
+            if (handDetector.landmarkDictionary[0][12][1]) <(handDetector.landmarkDictionary[0][9][1]):
+                handIsOpen = True
+                #circleC = (0, 255, 255)
+            else:
+                handIsOpen = False
+                #circleC = (255, 0, 0)
+
+            handTop = circleY - circleZ
+            handBottom = circleY + circleZ
+            handLeft = circleX - circleZ
+            handRight = circleX + circleZ
+
+            planetTop= planet.py - planet.radius
+            planetBottom= planet.py + planet.radius
+            planetLeft= planet.px - planet.radius
+            planetRight= planet.px + planet.radius
+
+            planet1Top= planet1.py - planet1.radius
+            planet1Bottom= planet1.py + planet1.radius
+            planet1Left= planet1.px - planet1.radius
+            planet1Right= planet1.px + planet1.radius
+            
+            
+            if planetLeft <= handRight and planetTop <= handBottom and planetRight >= handLeft and planetBottom >= handTop:
+                collision = True
+                circleC = (0, 255, 0)
+
+                if not handIsOpen:
+                    hold = True
+                    circleC = (0, 0, 255)
+                    planet.px= circleX
+                    planet.py = circleY
+
+            if planet1Left <= handRight and planet1Top <= handBottom and planet1Right >= handLeft and planet1Bottom >= handTop:
+                collision = True
+                circleC = (0, 255, 0)
+                
+                if not handIsOpen:
+                    hold = True
+                    circleC = (0, 0, 255)
+                    planet1.px= circleX
+                    planet1.py = circleY
+
+            else:
+                planet.acceleration(planet1)
+
+                    
+            '''
+            #check collision between rectangle and hand point 
+            if planet.collidepoint(circleX, circleY) or planet1.collidepoint(circleX, circleY):
+                rectColor = (255,255, 0)
+                if not handIsOpen:
+                    rectColor = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            else:
+                rectColor = (255, 0, 255)
+            '''
 
         # for all the game events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-               running = False
-               pass
+               running = False 
+        
 
-        WINDOW.fill((0,0,0))
         planet.render(WINDOW)
+        #planet.move()
+        planet1.render(WINDOW)
+        #planet1.move()
+        
 
+        pygame.draw.circle(WINDOW, circleC, (circleX, circleY), circleZ)
+        
         # this gets a list of booleans showing which keys are currently pressed
         keysPressed = pygame.key.get_pressed()
-
-        # put code here that should be run every frame
-         # of your game             
+            
         pygame.display.update()
-main()
+    
+    # Closes all the frames
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
